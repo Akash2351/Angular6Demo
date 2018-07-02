a4d3e493f441419ab293194a6dc6eb608aedd06e
Akash Anjanappa <akash2351@yahoo.co.in>
2018-07-01 20:05:28 -0400

    shallow integration tests

--- APM-UnitTesting/src/app/heroes/heroes.component.spec.ts
+++ APM-UnitTesting/src/app/heroes/heroes.component.spec.ts
@@ -1,42 +1,43 @@
 import { HeroesComponent } from "./heroes.component";
 import { of } from "rxjs/observable/of";
 
-describe('HeroesComponent',()=>{
-
+describe("HeroesComponent", () => {
   let component: HeroesComponent;
   let HEROES;
   let mockHeroService;
 
-  beforeEach(()=> {
+  beforeEach(() => {
     HEROES = [
-      { id: 1, name: 'SpiderMan', strength: 8},
-      { id: 2, name: 'ShakthiMan', strength: 3},
-      { id: 3, name: 'SnakeMan', strength: 11}
+      { id: 1, name: "SpiderMan", strength: 8 },
+      { id: 2, name: "ShakthiMan", strength: 3 },
+      { id: 3, name: "SnakeMan", strength: 11 }
     ];
 
-    mockHeroService = jasmine.createSpyObj(['getHeroes','addHero','deleteHero']);
+    mockHeroService = jasmine.createSpyObj([
+      "getHeroes",
+      "addHero",
+      "deleteHero"
+    ]);
     component = new HeroesComponent(mockHeroService);
-  })
+  });
 
-  describe('delete',()=>{
-
-    it('should remove the indicated hero from heroes list', ()=>{
+  describe("delete", () => {
+    it("should remove the indicated hero from heroes list", () => {
       component.heroes = HEROES;
       mockHeroService.deleteHero.and.returnValue(of(true));
 
       component.delete(HEROES[2]);
 
       expect(component.heroes.length).toBe(2);
-    })
+    });
 
-    it('should call deleteHero', ()=>{
+    it("should call deleteHero", () => {
       component.heroes = HEROES;
       mockHeroService.deleteHero.and.returnValue(of(true));
 
       component.delete(HEROES[2]);
 
       expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
-    })
-  })
-
-})
+    });
+  });
+});
