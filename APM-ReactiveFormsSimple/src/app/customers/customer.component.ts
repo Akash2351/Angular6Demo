b43f151655f6e50261cd9d881edf910663d2b6d8
Akash Anjanappa <akash2351@yahoo.co.in>
2018-07-07 14:28:22 -0400

    validation and cross field validation

--- APM-ReactiveFormsSimple/src/app/customers/customer.component.ts
+++ APM-ReactiveFormsSimple/src/app/customers/customer.component.ts
@@ -1,7 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { Customer } from './customer';
+
+
+function emailMatcher(c: AbstractControl): {[key: string]: boolean} | null {
+    let emailControl = c.get('email');
+    let confirmControl = c.get('confirmEmail');
+
+    if (emailControl.pristine || confirmControl.pristine) {
+      return null;
+    }
+
+    if (emailControl.value === confirmControl.value) {
+        return null;
+    }
+    return { 'match': true };
+ }
+
+function ratingRange(min: number, max: number): ValidatorFn {
+    return  (c: AbstractControl): {[key: string]: boolean} | null => {
+        if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return { 'range': true };
+        };
+        return null;
+    };
+}
 
 @Component({
     selector: 'my-signup',
@@ -15,10 +39,16 @@ export class CustomerComponent implements OnInit {
 
     ngOnInit(): void {
         this.customerForm = this.fb.group({
-            firstName: '',
-            lastName: { value: 'n/a', disabled: true},
-            email: '',
-            sendCatalog: true
+            firstName: ['',[Validators.required, Validators.minLength(3)]],
+            lastName:[ { value: 'n/a', disabled: true},[Validators.required, Validators.maxLength(50)]],
+            emailGroup: this.fb.group({
+                email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+                confirmEmail: ['', Validators.required],
+            },{validator: emailMatcher}),
+            sendCatalog: true,
+            phone: '',
+            notification: 'email',
+            rating: ['', ratingRange(1,5)]
         })
     }
 
@@ -34,5 +64,15 @@ export class CustomerComponent implements OnInit {
     save() {
         console.log(this.customerForm);
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        if (notifyVia === 'text') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
     }
  }
